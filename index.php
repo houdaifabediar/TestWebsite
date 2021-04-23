4e6bbea00e11d09046ab3d99b07420837b3ff802
houdaifabediar <82871963+houdaifabediar@users.noreply.github.com>
2021-04-23 15:47:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+</head>
+
+<body>
+<title>Test website, Houdaifa Bediar</title>
+
+<h1> Testing website voor CI/CD-tool </h1>
+
+<?php
+
+$host = "localhost";
+$username = "root";
+$user_pass = "usbw";
+$database_in_use = "test";
+ 
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+
+echo $mysqli->host_info . "\n";
+
+
+
+
+$sql = "SELECT Voornaam FROM gebruiker";
+$result = $mysqli -> query ($sql);
+
+if ($result -> num_rows > 0) {
+	//output van de data 
+	while ($row = $result -> fetch_assoc()) {
+		echo "voornaam: " . $row["Voornaam"]; 
+	}
+} else {
+	echo "no results";
+}
+
+$mysqli -> close();
+
+
+
+?>
+
+
+</body>
+
+<html>
